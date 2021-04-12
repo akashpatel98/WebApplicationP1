219a9621dd32a6fb211722fa42fe274bf297cf86
akashpatel98 <abp78@njit.edu>
2021-04-12 15:54:04 -0400

    Task: Added new Database

--- app/app.py
+++ app/app.py
@@ -5,7 +5,6 @@ from flask import render_template
 from flaskext.mysql import MySQL
 from mysql.cursors import DictCursor
 
-
 app = Flask(__name__)
 mysql = MySQL(cursorclass=DictCursor)
 
@@ -13,7 +12,7 @@ app.config['MYSQL_DATABASE_HOST'] = 'db'
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
 app.config['MYSQL_DATABASE_PORT'] = 3306
-app.config['MYSQL_DATABASE_DB'] = 'citiesData'
+app.config['MYSQL_DATABASE_DB'] = 'deniro'
 mysql.init_app(app)
 
 
