6955c22e3d4ea87e79aa0603d4fb4f54c93bbd41
akashpatel98 <abp78@njit.edu>
2021-05-01 15:37:56 -0400

    Task: Updated files for Part 3 and 4

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ import simplejson as json
 from flask import Flask, request, Response, redirect
 from flask import render_template
 from flaskext.mysql import MySQL
-from mysql.cursors import DictCursor
+from pymysql.cursors import DictCursor
 
 app = Flask(__name__)
 mysql = MySQL(cursorclass=DictCursor)
@@ -12,7 +12,7 @@ app.config['MYSQL_DATABASE_HOST'] = 'db'
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
 app.config['MYSQL_DATABASE_PORT'] = 3306
-app.config['MYSQL_DATABASE_DB'] = 'deniro'
+app.config['MYSQL_DATABASE_DB'] = 'dataDeniro'
 mysql.init_app(app)
 
 
@@ -20,15 +20,15 @@ mysql.init_app(app)
 def index():
     user = {'username': 'deniro Project'}
     cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM tbldeniroImport')
+    cursor.execute('SELECT * FROM ImportDeniro')
     result = cursor.fetchall()
     return render_template('index.html', title='Home', user=user, year=result)
 
 
 @app.route('/view/<int:year_id>', methods=['GET'])
-def record_view(city_id):
+def record_view(year_id):
     cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM tbldeiroImport WHERE id=%s', city_id)
+    cursor.execute('SELECT * FROM ImportDeniro WHERE id=%s', year_id)
     result = cursor.fetchall()
     return render_template('view.html', title='View Form', year=result[0])
 
@@ -36,7 +36,7 @@ def record_view(city_id):
 @app.route('/edit/<int:year_id>', methods=['GET'])
 def form_edit_get(year_id):
     cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM tbldeniroImport WHERE id=%s', year_id)
+    cursor.execute('SELECT * FROM ImportDeniro WHERE id=%s', year_id)
     result = cursor.fetchall()
     return render_template('edit.html', title='Edit Form', year=result[0])
 
@@ -46,7 +46,7 @@ def form_update_post(year_id):
     cursor = mysql.get_db().cursor()
     inputData = (request.form.get('intYear'), request.form.get('intScore'), request.form.get('fldTitle'),
                 year_id)
-    sql_update_query = """UPDATE tbldeniroImport  SET t.intYear = %s, t.intScore = %s, t.fldTitle = %s """
+    sql_update_query = """UPDATE ImportDeniro  SET t.intYear = %s, t.intScore = %s, t.fldTitle = %s """
     cursor.execute(sql_update_query, inputData)
     mysql.get_db().commit()
     return redirect("/", code=302)
@@ -61,7 +61,7 @@ def form_insert_get():
 def form_insert_post():
     cursor = mysql.get_db().cursor()
     inputData = (request.form.get('intYear'), request.form.get('intScore'), request.form.get('fldTitle'))
-    sql_insert_query = """INSERT INTO tbldeniroImport (intYear,intScore,fldTitle) VALUES (%s, %s,%s) """
+    sql_insert_query = """INSERT INTO ImportDeniro (intYear,intScore,fldTitle) VALUES (%s, %s,%s) """
     cursor.execute(sql_insert_query, inputData)
     mysql.get_db().commit()
     return redirect("/", code=302)
@@ -70,7 +70,7 @@ def form_insert_post():
 @app.route('/delete/<int:year_id>', methods=['POST'])
 def form_delete_post(year_id):
     cursor = mysql.get_db().cursor()
-    sql_delete_query = """DELETE FROM tbldeniroImport WHERE id = %s """
+    sql_delete_query = """DELETE FROM ImportDeniro WHERE id = %s """
     cursor.execute(sql_delete_query, year_id)
     mysql.get_db().commit()
     return redirect("/", code=302)
@@ -79,7 +79,7 @@ def form_delete_post(year_id):
 @app.route('/api/v1/deniro', methods=['GET'])
 def api_browse() -> str:
     cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM tbldeniroImport')
+    cursor.execute('SELECT * FROM ImportDeniro')
     result = cursor.fetchall()
     json_result = json.dumps(result);
     resp = Response(json_result, status=200, mimetype='application/json')
@@ -89,7 +89,7 @@ def api_browse() -> str:
 @app.route('/api/v1/deniro/<int:year_id>', methods=['GET'])
 def api_retrieve(year_id) -> str:
     cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM tbldeniroImport WHERE id=%s', year_id)
+    cursor.execute('SELECT * FROM ImportDeniro WHERE id=%s', year_id)
     result = cursor.fetchall()
     json_result = json.dumps(result);
     resp = Response(json_result, status=200, mimetype='application/json')
@@ -101,7 +101,7 @@ def api_edit(year_id) -> str:
     cursor = mysql.get_db().cursor()
     content = request.json
     inputData = (content['intYear'], content['intScore'], content['fldTitle'], year_id)
-    sql_update_query = """UPDATE tbldeniroImport t SET t.intYear = %s, t.intScore = %s, t.fldTitle = %s, WHERE t.id = %s """
+    sql_update_query = """UPDATE ImportDeniro t SET t.intYear = %s, t.intScore = %s, t.fldTitle = %s, WHERE t.id = %s """
     cursor.execute(sql_update_query, inputData)
     mysql.get_db().commit()
     resp = Response(status=200, mimetype='application/json')
@@ -114,7 +114,7 @@ def api_add() -> str:
 
     cursor = mysql.get_db().cursor()
     inputData = (content['intYear'], content['intScore'], content['fldTitle'])
-    sql_insert_query = """INSERT INTO tbldeniroImport (intYear,intScore,fldTitle) VALUES (%s, %s,%s) """
+    sql_insert_query = """INSERT INTO ImportDeniro (intYear,intScore,fldTitle) VALUES (%s, %s,%s) """
     cursor.execute(sql_insert_query, inputData)
     mysql.get_db().commit()
     resp = Response(status=201, mimetype='application/json')
@@ -124,7 +124,7 @@ def api_add() -> str:
 @app.route('/api/v1/deniro/<int:year_id>', methods=['DELETE'])
 def api_delete(city_id) -> str:
     cursor = mysql.get_db().cursor()
-    sql_delete_query = """DELETE FROM tbldeniroImport WHERE id = %s """
+    sql_delete_query = """DELETE FROM ImportDeniro WHERE id = %s """
     cursor.execute(sql_delete_query, city_id)
     mysql.get_db().commit()
     resp = Response(status=200, mimetype='application/json')
